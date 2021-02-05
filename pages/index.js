4d9dff7c2bece5f6e92d39a1a17ad4487fc180f8
Dhadhazi <dhadhazi@gmail.com>
2021-02-05 15:02:24 +0100

    Stripe checkout added

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
 import products from "../products.json";
+import { initiateCheckout } from "../lib/payments";
 
 export default function Home() {
   return (
@@ -29,6 +30,23 @@ export default function Home() {
                   <p>{price}</p>
                   <p>{description}</p>
                 </a>
+                <p>
+                  <button
+                    className={styles.button}
+                    onClick={() => {
+                      initiateCheckout({
+                        lineItems: [
+                          {
+                            price: id,
+                            quantity: 1,
+                          },
+                        ],
+                      });
+                    }}
+                  >
+                    Buy Now
+                  </button>
+                </p>
               </li>
             );
           })}
