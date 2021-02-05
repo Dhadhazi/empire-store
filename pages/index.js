9a6b2e5ca6121f34ad1f8f7cac52b17735ca34e5
Dhadhazi <dhadhazi@gmail.com>
2021-02-05 14:16:04 +0100

    Products added

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,53 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Empire Store</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Empire Store</h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Welcome to the best store in the Galactic Empire!
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <ul className={styles.grid}>
+          <li className={styles.card}>
+            <a href="https://nextjs.org/docs">
+              <img src="/images/dvhelmet.jpg" alt="Darth Vader Helmet" />
+              <h3>Darth Vader Helmet</h3>
+              <p>
+                Fans can portray the greatest battles and missions from the Star
+                Wars saga with the helmets from The Black Series!{" "}
+              </p>
+            </a>
+          </li>
+          <li className={styles.card}>
+            <a href="https://nextjs.org/learn">
+              <img src="/images/atat.jpg" alt="LEGO AT-AT" />
+              <h3>LEGO Star Wars AT-AT</h3>
+              <p>
+                The buildable, posable AT-AT Walker comes with Luke Skywalker,
+                General Veers and other LEGO minifigures.
+              </p>
+            </a>
+          </li>
+          <li className={styles.card}>
+            <a href="https://github.com/vercel/next.js/tree/master/examples">
+              <img src="/images/metaldeathstar.jpg" alt="Metal Death Star" />
+              <h3> Death Star 7.5 Cm &rarr;</h3>
+              <p>
+                Take on the challenge of building your own detailed Death Star
+                with this laser-cut Metal Earth 3D model kit!{" "}
+              </p>
+            </a>
+          </li>
+        </ul>
       </main>
 
       <footer className={styles.footer}>
@@ -56,10 +56,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
